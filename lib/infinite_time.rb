b68916d245d93abb28b4ce0112d8ba4ea67f8b19
Andrew Marshall <andrew@johnandrewmarshall.com>
2013-03-14 22:51:21 -0400

    Override all Time methods that don't make sense
    Which is mostly all of them. Hopefully didn't miss any.

--- lib/infinite_time.rb
+++ lib/infinite_time.rb
@@ -2,6 +2,15 @@ require 'infinite_time/version'
 require 'infinite_time/core_ext'
 
 class InfiniteTime < Time
+  class << self
+    undef at
+    undef gm
+    undef local
+    undef mktime
+    undef now
+    undef utc
+  end
+
   def initialize sign = :+
     sign = sign.to_sym
     raise InvalidSign unless [:+, :-].include? sign
@@ -34,5 +43,72 @@ class InfiniteTime < Time
   def + _; self; end
   def - _; self; end
 
+  def hash
+    self.class.hash ^ @sign.hash
+  end
+
+  def to_i; raise TypeError, 'InfiniteTime has no Integer representation'; end
+  def to_r; raise TypeError, 'InfiniteTime has no Rational representation'; end
+
+  def to_s
+    "#{@sign}∞"
+  end
+  alias_method :asctime, :to_s
+  alias_method :ctime, :to_s
+  alias_method :inspect, :to_s
+
+  def strftime _; to_s; end
+
+  private
+
+  def signed_float
+    (positive?) ? Float::INFINITY : -Float::INFINITY
+  end
+
   class InvalidSign < StandardError; end
+
+  module TimeCompatability
+    def self.define_method name, arity = 0, &block
+      super name do |*args|
+        unless arity === args.length
+          raise ArgumentError, "wrong number of arguments (#{args.length} for #{arity})"
+        end
+        instance_exec(*args, &block)
+      end
+    end
+
+    def self.nil_method name, arity = 0
+      define_method(name, arity) { nil }
+    end
+    def self.nil_methods names
+      names.each { |name| nil_method name }
+    end
+
+    def self.signed_method name, arity = 0
+      define_method(name, arity) { signed_float }
+    end
+    def self.signed_methods names
+      names.each { |name| signed_method name }
+    end
+
+    def self.self_method name, arity = 0
+      define_method(name, arity) { self }
+    end
+    def self.self_methods names
+      names.each { |name| self_method name }
+    end
+
+    signed_methods %i[year month day yday wday hour minute second subsec nsec usec]
+    nil_methods %i[monday? tuesday? wednesday? thursday? friday? saturday? sunday?]
+    nil_methods %i[dst? isdst gmt? gmt_offset gmtoff utc? utc_offset zone]
+    self_method :round, 0..1
+    self_method :succ
+    alias_method :mday, :day
+    alias_method :mon, :month
+    alias_method :sec, :second
+    alias_method :tv_nsec, :nsec
+    alias_method :tv_usec, :usec
+  end
+  private_constant :TimeCompatability
+  include TimeCompatability
 end
--- spec/infinite_time_spec.rb
+++ spec/infinite_time_spec.rb
@@ -21,13 +21,141 @@ describe InfiniteTime do
   it "returns an unchanged self when math is performed on it" do
     itime = InfiniteTime.new
     (itime + 1).should eql itime
+    (itime + 1).should equal itime
     (itime - 1).should eql itime
+    (itime - 1).should equal itime
+    itime.round.should eql itime
+    itime.round.should equal itime
+    itime.round(0).should eql itime
+    itime.round(0).should equal itime
+    itime.succ.should eql itime
+    itime.succ.should equal itime
+  end
+
+  it "returns an unchanged self when timezone conversion is performed on it" do
+    itime = InfiniteTime.new
+    itime.getgm.should eql itime
+    itime.getutc.should eql itime
+    itime.getlocal.should eql itime
+    itime.getlocal('+01:00').should eql itime
+    itime.localtime.should eql itime
+    itime.localtime('+01:00').should eql itime
+
+    itime.gmtime.should eql itime
+    itime.utc.should eql itime
   end
 
   it "cannot be compared against anything but Time objects" do
     expect { InfiniteTime.new <=> Object.new }.to raise_error ArgumentError
     expect { InfiniteTime.new <=> Date.new }.to raise_error ArgumentError
     expect { InfiniteTime.new <=> 321 }.to raise_error ArgumentError
+  end
+
+  describe "time compatibility" do
+    it "returns nil for all day of the week predicate methods" do
+      positive = InfiniteTime.new :+
+      negative = InfiniteTime.new :-
+
+      [positive, negative].each do |itime|
+        itime.monday?.should == nil
+        itime.tuesday?.should == nil
+        itime.wednesday?.should == nil
+        itime.thursday?.should == nil
+        itime.friday?.should == nil
+        itime.saturday?.should == nil
+        itime.sunday?.should == nil
+      end
+    end
+
+    it "returns nil for all time zone inquiry methods" do
+      positive = InfiniteTime.new :+
+      negative = InfiniteTime.new :-
+
+      [positive, negative].each do |itime|
+        itime.dst?.should == nil
+        itime.isdst.should == nil
+
+        itime.gmt?.should == nil
+        itime.utc?.should == nil
+        itime.zone.should == nil
+
+        itime.gmt_offset.should == nil
+        itime.gmtoff.should == nil
+        itime.utc_offset.should == nil
+      end
+    end
+
+    it "returns the appropriate infinity for time/date inquiry methods" do
+      positive = InfiniteTime.new :+
+      negative = InfiniteTime.new :-
+
+      positive.year.should    == +Float::INFINITY
+      positive.month.should   == +Float::INFINITY
+      positive.mon.should     == +Float::INFINITY
+      positive.day.should     == +Float::INFINITY
+      positive.yday.should    == +Float::INFINITY
+      positive.mday.should    == +Float::INFINITY
+      positive.wday.should    == +Float::INFINITY
+      positive.hour.should    == +Float::INFINITY
+      positive.minute.should  == +Float::INFINITY
+      positive.second.should  == +Float::INFINITY
+      positive.sec.should     == +Float::INFINITY
+      positive.subsec.should  == +Float::INFINITY
+      positive.nsec.should    == +Float::INFINITY
+      positive.usec.should    == +Float::INFINITY
+      positive.tv_nsec.should == +Float::INFINITY
+      positive.tv_usec.should == +Float::INFINITY
+
+      negative.year.should    == -Float::INFINITY
+      negative.month.should   == -Float::INFINITY
+      negative.mon.should     == -Float::INFINITY
+      negative.day.should     == -Float::INFINITY
+      negative.yday.should     == -Float::INFINITY
+      negative.mday.should    == -Float::INFINITY
+      negative.wday.should    == -Float::INFINITY
+      negative.hour.should    == -Float::INFINITY
+      negative.minute.should  == -Float::INFINITY
+      negative.second.should  == -Float::INFINITY
+      negative.sec.should     == -Float::INFINITY
+      negative.subsec.should  == -Float::INFINITY
+      negative.nsec.should    == -Float::INFINITY
+      negative.usec.should    == -Float::INFINITY
+      negative.tv_nsec.should == -Float::INFINITY
+      negative.tv_usec.should == -Float::INFINITY
+    end
+
+    it "returns a TypeError for impossible numeric conversions" do
+      itime = InfiniteTime.new
+
+      expect { itime.to_i }.to raise_error TypeError
+      expect { itime.to_r }.to raise_error TypeError
+    end
+
+    it "has sensible string representations" do
+      positive = InfiniteTime.new :+
+      negative = InfiniteTime.new :-
+
+      plus_∞ = '+∞'
+      minus_∞ = '-∞'
+
+      positive.asctime.should      == plus_∞
+      positive.ctime.should        == plus_∞
+      positive.inspect.should      == plus_∞
+      positive.strftime('').should == plus_∞
+      positive.to_s.should         == plus_∞
+
+      negative.asctime.should      == minus_∞
+      negative.ctime.should        == minus_∞
+      negative.inspect.should      == minus_∞
+      negative.strftime('').should == minus_∞
+      negative.to_s.should         == minus_∞
+    end
+
+    (Time.methods - Time.superclass.methods).each do |method|
+      it "undefines the class method '#{method}' since it makes no sense" do
+        expect { InfiniteTime.public_send method }.to raise_error NoMethodError
+      end
+    end
   end
 
   describe "a positively infinite time" do
@@ -51,6 +179,7 @@ describe InfiniteTime do
       a, b = InfiniteTime.new(:+), InfiniteTime.new(:+)
       (a == b).should == true
       (a.eql? b).should == true
+      (a.equal? b).should == false
     end
 
     it "is greater than a negatively infinite time" do
@@ -82,6 +211,7 @@ describe InfiniteTime do
       a, b = InfiniteTime.new(:-), InfiniteTime.new(:-)
       (a == b).should == true
       (a.eql? b).should == true
+      (a.equal? b).should == false
     end
 
     it "is less than a positively infinite time" do
